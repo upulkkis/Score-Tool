9dee740261fb164ac633aa1da7b46ce5f302d403
Uljas Pulkkis <uljas.pulkkis@gmail.com>
2020-08-18 10:50:37 +0300

    setup reqs

--- setup.py
+++ setup.py
@@ -3,21 +3,44 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = [
+    'gunicorn>=19.7.1',
+    'plotly>=2.0.9',
+    'dash>=0.17.4',
+    'dash-renderer>=0.7.2',
+    'dash-html-components>=0.6.0',
+    'dash-core-components>=0.5.0',
+    'dash_daq>=0.5.0',
+    'dash_bootstrap_components==0.10.0',
+    'dash_admin_components==0.1.4',
+    'score-component==0.4.9',
+    'pretty_midi==0.2.9',
+    'librosa==0.7.2',
+    'visdcc==0.0.40',
+    'music21==5.1.0'
+]
+
 setuptools.setup(
     name="orchestration-analyzer", # Replace with your own username
     version="0.5",
     author="Uljas Pulkkis",
     author_email="uljas.pulkkis@gmail.com",
+    license='MIT',
     description="An orchestration analysis tool for composers, conductors, musicians and music theorists",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/upulkkis/orchestration-analyzer",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 	scripts=['bin.app'],
+    entry_points="""
+    orchan=bin.app:app.run_server(debug=False, threaded=True)
+    """,
     python_requires='>=3.7.6',
 )
